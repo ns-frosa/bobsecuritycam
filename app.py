a215b304afceeafb1c93a58593a7e623755ccfb3
Fabio Rosa <frosa@netskope.com>
2024-12-19 23:20:37 -0500

    updates

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+    app.run(host='0.0.0.0', port=5001, threaded=True)
